6695db6b79ccdbdda04a6a90beac011d561dd713
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-28 12:10:27 +0500

    set routes

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+// Components
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import SingleProduct from "./pages/SingleProduct";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import FAQ from "./pages/FAQ";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct" element={<SingleProduct />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
