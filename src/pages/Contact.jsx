e410b5a7278321b8f088ee5bf6b9a907c293cbdf
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-31 19:50:08 +0500

    error handle image is not found

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div>
+        <h2>Get In Touch With Us</h2>
+        <p>For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!</p>
+      </div>
+    </div>
   )
 }
 
